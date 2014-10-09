1ce8a6aaf653e9179186db1a26a7e12955ac66b9
Luke Rebarchik <myfavoriteluke@gmail.com>
2014-10-09 15:29:21 -0600

    Fixed Foundation compass compile (4)

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-require "zurb-foundation"
+add_import_path "bower_components/foundation/scss"
 #add_import_path "sass/foundation/bower_components"
 
 # Set this to the root of your project when deployed:
